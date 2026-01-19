80b9f195c7e64faaea801feeb88e046889348281
Dan Rosén <danr42@gmail.com>
2026-01-19 13:15:55 +0100

    Set PYTHONUNBUFFERED in software/hcs_with_web_service.sh

--- software/hcs_with_web_service.sh
+++ software/hcs_with_web_service.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export squid_web_service=0.0.0.0:5050
 cd ~/squid/software
-python3 main_hcs.py 2>&1 | tee "log-$(date --iso-8601=seconds).stdio.txt" 
-sleep 10
-
+PYTHONUNBUFFERED=x squid_web_service=0.0.0.0:5050 python3 main_hcs.py 2>&1 | tee "log-$(date --iso-8601=seconds).stdio.txt"
