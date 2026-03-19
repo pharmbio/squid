8612504aa1a333cbe008735f788909648352b4e0
Dan Rosén <danr42@gmail.com>
2026-03-19 09:34:48 +0100

    Store the UV venv outside the project directory

--- software/hcs_with_web_service.sh
+++ software/hcs_with_web_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/squid/software
-PYTHONUNBUFFERED=x squid_web_service=0.0.0.0:5050 uv run python main_hcs.py 2>&1 | tee "log-$(date --iso-8601=seconds).stdio.txt"
+UV_PROJECT_ENVIRONMENT="$HOME/.venvs/squid" PYTHONUNBUFFERED=x squid_web_service=0.0.0.0:5050 uv run python main_hcs.py 2>&1 | tee "log-$(date --iso-8601=seconds).stdio.txt"
